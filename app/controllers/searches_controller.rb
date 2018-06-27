080fee0a59151f459900bf482d2ddaf562a8348e
Sara Foresti <Sara.foresti@hotmail.com>
2018-06-27 18:30:50 +0100

    search model implemented

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,11 @@
+class SearchesController < ApplicationController
+  def index
+    # if params[:query].present?
+    #   call the API
+    # else
+    #   print "enter an artist please"
+    # end
+  end
+
+
+end
